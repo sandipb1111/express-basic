8192b24e254e685592ad4a909c455b17634ba357
Sandip <sandip.basnet1111@gmail.com>
2023-05-26 08:07:27 +0545

    Database

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { config } from "dotenv"
+import cors from "cors"
+import express, { Request, Response } from "express"
+import crypto from "crypto"
+
+config()
+
+const app = express()
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use("/", (req: Request, res: Response) => {
+    res.send("Hello World")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`)
+})
